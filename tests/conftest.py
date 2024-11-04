854a602e10ba1da90d39893cc05ec9079674cf0a
Stefane Fermigier <sf@abilian.com>
2024-11-04 19:05:16 +0100

    chore: deps

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,13 @@ from flask import Flask
 from flask.testing import FlaskCliRunner
 
 
-@pytest.fixture()
+@pytest.fixture
 def app() -> Flask:
     app = Flask(__name__)
     app = svcs.flask.init_app(app)
     return app
 
 
-@pytest.fixture()
+@pytest.fixture
 def runner(app: Flask) -> FlaskCliRunner:
     return app.test_cli_runner()
